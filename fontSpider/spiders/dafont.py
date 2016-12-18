c1036b839325334e2a709d046b4178f6b1ab94cf
evoup <evoex123@gmail.com>
2016-12-18 19:03:29 +0800

    next page implement

--- fontSpider/spiders/dafont.py
+++ fontSpider/spiders/dafont.py
@@ -12,8 +12,9 @@ class DafontSpider(scrapy.Spider):
         for href in response.css('a.dl::attr(href)').extract():
             print "[hit:" + href + "]"
             #超链接的title为Keyboard shortcut: Right arrow
-        #next_page = response.css('a[title=]').extract_first()
-        #if next_page is not None:
-            #next_page = response.urljoin(next_page)
-            #yield scrapy.Request(next_page, callback=self.parse)
+        next_page = response.css("a[title='Keyboard shortcut: Right arrow']::attr(href)").extract_first()
+        print "next_page" + next_page
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
         pass
