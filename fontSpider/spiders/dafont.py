f403d4061e7ed32dd73114d9bc8b986d6699c576
evoup <evoex123@gmail.com>
2016-12-18 16:53:36 +0800

    c

--- fontSpider/spiders/dafont.py
+++ fontSpider/spiders/dafont.py
@@ -5,10 +5,15 @@ import scrapy
 class DafontSpider(scrapy.Spider):
     name = "dafont"
     allowed_domains = ["dafont.com"]
-    start_urls = ['http://dafont.com/']
+    start_urls = ['http://dafont.com/new.php']
 
     def parse(self, response):
         print "--------------------------------------------------"
         for href in response.css('a.dl::attr(href)').extract():
             print "[hit:" + href + "]"
+            #超链接的title为Keyboard shortcut: Right arrow
+        #next_page = response.css('a[title=]').extract_first()
+        #if next_page is not None:
+            #next_page = response.urljoin(next_page)
+            #yield scrapy.Request(next_page, callback=self.parse)
         pass
